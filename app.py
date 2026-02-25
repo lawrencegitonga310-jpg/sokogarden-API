d7cc725ed53e7738cd976c0214aff9fc4c7b464e
lawrencegitonga310-jpg <lawrencegitonga310@gmail.com>
2026-02-24 19:00:28 -1200

    added update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # import flask and its components
 from flask import*
+import os
 
 # import a  pymysql module it helps to create a connection btn python flask and mysql database
 import pymysql
@@ -7,8 +8,10 @@ import pymysql
 # crate a flask application
 app = Flask(__name__)
 
+# configure the location where your Products images will be saved on your application
+app.config["UPLOAD_FOLDER"]="static/images"
 
-# below is the signup route
+# below is the signup route(registration)
 @app.route("/api/signup",methods=["POST"])
 def signup():
      if request.method=="POST":
@@ -42,7 +45,7 @@ def signup():
 
      return jsonify({"message":"User register successfully"})
 
-@app.route("/api/signup",methods=["POST"])
+@app.route("/api/signin",methods=["POST"])
 def signin():
      if request.method=="POST":
         #  extract the two details entered on the form
@@ -59,7 +62,7 @@ def signin():
         cursor = connection.cursor(pymysql.cursors.DictCursor)
 
         # structure the sql query that will check if the email and password entered are correct
-        sql ="SELECT* FROM users WHERE email=%s AND password=%s)"
+        sql ="SELECT* FROM users WHERE email=%s AND password=%s"
         
         # Put the data received from the form into a tuple
         data = (email,password)
@@ -80,7 +83,77 @@ def signin():
             # return the details to the fronted as well as a message
             return jsonify({"message":"User logged in successfully", "user":user})
 
+ # below is a route for adding products
+@app.route("/api/add_products",methods=["POST"])
+def add_products():
+    if request.method == "POST":
+        # extract the data entered on the form 
+        product_name =request.form["product_name"]
+        product_description = request.form["product_description"]
+        product_cost = request.form["product_cost"]
+        # for the product photo we shall fetch it from the file as shown below
+        product_photo =request.files["product_photo"]
+
+        # extract the the file name of the product photo
+        filename= product_photo.filename
+        # by use of os module we can extract the file path where the images is currently saved
+        photo_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         
+        # print("this is the photo path:",photo_path)
+        # save the product phot image into the new location
+        product_photo.save(photo_path)
+
+        # print them out to test whether you are receiving the details sent with the request
+        # print(product_name, product_description, product_cost, product_photo) 
+
+        # establish a connection to the db
+        connection = pymysql.connect(host="localhost", user="root", password= "", database= "sokogardenonline")
+
+        # create cursor
+        cursor = connection.cursor()
+
+        # structure the sql query to insert the product details to the database
+        sql = "INSERT INTO product_details(product_name, product_description, product_cost, product_photo) VALUES (%s,%s,%s, %s)"
+       
+      #  create a tuple that will hold the data from which are current held onto the different variables declared
+    data = (product_name, product_description, product_cost, filename)
+
+    # use the cursor to execute the sql as you replace the placeholders with the actual data
+    cursor.execute(sql, data)
+
+    # commit the changes to database 
+    connection.commit()
+
+
+    return jsonify({"message" : "Add product route accessed"})
+    
+# below is a route for fetching products
+@app.route("/api/get_products")
+def get_products():
+    # create a connection to db 
+   connection = pymysql.connect(host="localhost", user="root", password= "", database= "sokogardenonline")
+
+    # create a cursor
+   cursor= connection.cursor()
+
+  # structure the query to fetch all the products from the table products_details
+   sql = "SELECT * FROM product_details"
+
+
+# execute the query
+   cursor.execute(sql)
+
+# create a variable that hold the data fetched from the table
+   products = cursor.fetchall()
+
+
+
+
+
+   return jsonify(products)
+
+
+
 # run the web app
 app.run(debug=True)
 
